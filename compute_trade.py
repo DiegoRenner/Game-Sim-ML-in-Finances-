fb754ebd32732ef6835ce416b75cd46eeb371eca
DiegoRenner <diego.renner@hotmail.com>
2021-05-09 13:00:33 +0200

    updated trading algorithm to fit task description

--- compute_trade.py
+++ compute_trade.py
@@ -5,18 +5,17 @@ import compute_utility as cu
 def compute_trade(N, r, d, w, s, D):
     w_new = np.copy(w)
     s_new = np.copy(s)
-    while(1):
+
+    # iterate over agnets in random order
+    perm = np.random.permutation(N)
+    for i in perm:
         U = cu.returns_ut(0,r,d,D,w_new,s_new)
-        # select random agent
-        i = int(np.floor(np.random.random(1) * N))
         # select agent with lowest utility
         j = int(np.argmin(U))
         # determine if there will be trade
         if ((U[j] <= U[i]) & (i!=j)):
             # determine number of stocks agent i can buy
             stocks_to_buy = np.floor(w_new[i]/D)
-            if((stocks_to_buy==0) | (s_new[j]==0)):
-                return w_new, s_new
             # trade depending on if there is more money than stock available or vice versa
             if  stocks_to_buy >= s_new[j]:
                 s_new[i] += s_new[j]
@@ -28,5 +27,4 @@ def compute_trade(N, r, d, w, s, D):
                 w_new[j] += D*stocks_to_buy
                 w_new[i] -= D*stocks_to_buy
                 s_new[j] -= stocks_to_buy
-        else:
-            return w_new, s_new
+    return w_new, s_new
