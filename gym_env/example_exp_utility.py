535942a328ea8e6d508fd8bdacc661d3a7de7b9c
Konrad Mueller <mail@konrad-mueller.com>
2021-05-10 11:09:45 +0200

    Added run function to run the game multiple times (without training).

--- gym_env/example_exp_utility.py
+++ gym_env/example_exp_utility.py
@@ -1,11 +1,11 @@
 # Example of interacting with environment using Exponential Utility Agents, constant time series, equal endowments
 
 from env import Agent, MarketEnv
-from utils import create_constant_series, ExpUtilityAgent
+from utils import create_constant_series, ExpUtilityAgent, run
 import numpy as np
 import pandas as pd
 
-EPISODES = 1
+RUNS = 1
 STEPS = 11
 CONSTANT_INTEREST = 0.05
 CONSTANT_DIVIDEND = 1
@@ -20,16 +20,7 @@ agents = [ExpUtilityAgent(i, RISK_AVERSIONS[i]) for i in np.arange(AGENT_NUM)]
 cash_endowments = [CASH_ENDOWMENT] * AGENT_NUM
 stock_endowments = [STOCK_ENDOWMENT] * AGENT_NUM
 
-env = MarketEnv(interest_data=interest_data, dividend_data=dividend_data, agents=agents, endowments_cash=cash_endowments, endowments_stock=stock_endowments)
+params = [interest_data, dividend_data, agents, cash_endowments, stock_endowments]
+trade_hist, final_states, final_rewards = run(RUNS, params)
 
-for i in range(STEPS):
-    total_returns = env.get_returns()
-    actions = [agent.act(c) for agent, c in zip(env.agents, total_returns)]
-    state, done, rewards, info = env.step(actions)
-
-for j in range(STEPS):
-    print(f'Trades at time {j}:')
-    for trade in env.trade_history[j]:
-        print(f'Buyer: {trade.agent_buy}, Seller: {trade.agent_sell}, Quantity: {trade.quantity}, Price: {trade.price}')
-
-print(f'Rewards: {rewards}')
+print(f'Rewards: {final_rewards}')
--- gym_env/utils.py
+++ gym_env/utils.py
@@ -19,3 +19,20 @@ def create_constant_series(interest_rate, dividend, steps):
     interest_data = pd.Series([interest_rate] * steps)
     dividend_data = pd.Series([dividend] * steps)
     return interest_data, dividend_data
+
+
+def run(runs, params):
+    """Runs the game for specified number of runs without training."""
+    trade_hist = []
+    final_states = []
+    final_rewards = []
+    for i in np.arange(runs):
+        env = MarketEnv(*params)
+        for step in np.arange(env.end_time):
+            total_returns = env.get_returns()
+            actions = [agent.act(c) for agent, c in zip(env.agents, total_returns)]
+            state, done, rewards, info = env.step(actions)
+        trade_hist.append(env.trade_history)
+        final_states.append(state)
+        final_rewards.append(rewards)
+    return trade_hist, final_states, final_rewards
