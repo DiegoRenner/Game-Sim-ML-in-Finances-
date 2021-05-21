de5e96bae330725fc3aeabb1c234d7b6da186eed
Konrad Mueller <mail@konrad-mueller.com>
2021-05-21 16:51:32 +0200

    Implementation of differential evolution optimizer

---
+++ full_tf_model/env_total_v2.py
@@ -0,0 +1,117 @@
+import sys
+import numpy as np
+from sortedcontainers import SortedList
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense
+from utils import Order, Trade
+
+
+class Game(keras.Model):
+
+    def __init__(self, end_time, agent_num, fc1_dims=128):
+        super(Game, self).__init__()
+
+        self.time = 0
+        self.end_time = end_time
+        self.agent_num = agent_num
+        self.fc1_dims = fc1_dims
+        self.out_dims = 1
+
+        # Tensors that unpack the input
+        self.data = tf.Variable(tf.zeros(2 * self.end_time), trainable=False)
+        self.book = tf.Variable(tf.zeros(2 * self.agent_num), trainable=False)
+
+        # Layer that calculates the returns of each agent
+        self.returns = tf.keras.layers.Lambda(self.get_returns)
+
+        # Initializing the layers corresponding to different agents
+        self.first_layers = [Dense(self.fc1_dims, activation='relu') for i in np.arange(self.agent_num)]
+        self.out_layers = [Dense(self.out_dims) for i in np.arange(self.agent_num)]
+
+        # Initialize remaining tensors needed in call function
+        self.intermediate_outputs = [tf.Variable(tf.zeros(self.fc1_dims)) for i in np.arange(self.agent_num)]
+        self.prices = [tf.Variable(tf.zeros(1)) for i in np.arange(self.agent_num)]
+
+    def get_returns(self, input):
+        """
+        Calculates current (wrt. time) returns of the agents
+        @param input: List containing the data and book tensors
+        @return: tensor containing the returns
+        """
+        data, book = input[0], input[1]
+        cash_return = tf.math.scalar_mul(data[self.time], book[:self.agent_num])
+        div_return = tf.math.scalar_mul(data[self.end_time + self.time], book[self.agent_num:])
+        return tf.math.add(cash_return, div_return)
+
+    def _payout_returns(self):
+        returns = self.get_returns([self.data, self.book])
+        stocks = self.book[self.agent_num:]
+        self.book.assign(tf.concat([tf.math.add(self.book[:self.agent_num], returns), stocks], axis=0))
+
+    def get_final_cash(self):
+        r = self.data[self.end_time - 1].numpy()
+        d = self.data[-1].numpy()
+        rate = np.multiply(d, np.divide(1 + r, r))
+        return self.book[:self.agent_num] + tf.math.scalar_mul(rate, self.book[self.agent_num:2 * self.agent_num])
+
+    def _execute_trade(self, trade):
+        _book = self.book.numpy()
+        _book[trade.agent_buy] -= trade.price
+        _book[trade.agent_sell] += trade.price
+        _book[self.agent_num + trade.agent_buy] += trade.quantity
+        _book[self.agent_num + trade.agent_sell] += trade.quantity
+
+        self.book.assign(tf.convert_to_tensor(_book))
+
+    def trade(self, prices):
+        """Trading as described in Algorithm 1"""
+        orders = SortedList(key=lambda x: x.price)
+
+        for i in np.arange(self.agent_num):
+            orders.add(Order(agent_id=i, price=prices[i]))
+
+        trades = []
+
+        while len(orders) > 1:
+            # Draw random agent's action
+            idx_bid = np.random.randint(len(orders))
+            bid = orders.pop(idx_bid)
+
+            cash_left = self.book[bid.agent_id].numpy()
+            ask = orders[0]
+            _seller_stocks = self.book[self.agent_num + ask.agent_id].numpy()
+
+            if bid.price >= ask.price and cash_left >= ask.price and _seller_stocks > 1:
+                _quantity = min(np.floor(cash_left / ask.price), _seller_stocks)
+                _trade = Trade(bid.agent_id, ask.agent_id, _quantity, ask.price)
+
+                self._execute_trade(_trade)
+                trades.append(_trade)
+
+    def call(self, x):
+        """
+        Call function of the model
+        @param x: tensor of interest_data, dividend_data, endowments_cash, endowments_stock
+        @return: rewards of each agent
+        """
+
+        # Unpacking the input tensor
+        self.data.assign(x[:2*self.end_time])
+        self.book.assign(x[2*self.end_time:])
+
+        for time in np.arange(self.end_time):
+            self.time = time
+            returns = self.returns([self.data, self.book])
+
+            # Forward pass of returns through the NN of each agent
+            for i in np.arange(self.agent_num):
+                self.intermediate_outputs[i].assign(tf.squeeze(self.first_layers[i](tf.reshape(returns[i], shape=(1, 1)))))
+                self.prices[i].assign(tf.reshape(self.out_layers[i](tf.reshape(self.intermediate_outputs[i],
+                    shape=(1, self.fc1_dims))), shape=(1,)))
+
+            self.trade(self.prices)
+            self._payout_returns()
+
+        return self.get_final_cash()  # Return final wealth as rewards
+
---
+++ full_tf_model/train.py
@@ -0,0 +1,83 @@
+import numpy as np
+import datetime
+from tqdm import trange
+import tensorflow as tf
+import tensorflow_probability as tfp
+from env_total_v2 import Game
+from utils import create_input_batch
+
+
+# Hyperparameters
+EPOCHS = 5
+STEPS = 11
+IRM_POOL = [0.01, 0.02, 0.05]
+IRS_POOL = [0.0]
+DIVM_POOL = [0.5, 1, 1.5]
+DIVS_POOL = [0.2, 0.3, 0.4]
+AGENT_NUM = 2
+ENDOW_CASH_POOL = [20, 100, 80]
+ENDOW_STOCK_POOL = [10, 15, 10]
+POPULATION_SIZE = 50  # number of different weight specifications the opt. algorithm compares
+MAX_ITERATIONS = 2  # number of iterations that the optimization algorithm runs
+save_model = False
+
+# Batch of game scenarios
+batch = create_input_batch(batch_size=EPOCHS, agent_num=AGENT_NUM, steps=STEPS,
+                           irm_pool=IRM_POOL, divm_pool=DIVM_POOL, irs_pool=IRS_POOL, divs_pool=DIVS_POOL,
+                           endow_cash_pool=ENDOW_CASH_POOL, endow_stock_pool=ENDOW_STOCK_POOL)
+
+game = Game(STEPS, AGENT_NUM)
+
+# Summary and dummy pass to ensure initialization
+x = batch[0]
+_ = game(x)  
+print(game.summary())
+
+iterations = 0
+
+# Training
+def objective_fn(w1, b1, w2, b2):
+    """
+    Wrapper function around game for training a specific agent. Input to the optimization algorithm.
+    each input is tensor where the first dimension indexes the elements in the population.
+    each population element contains weights / biases of layers of agent's NN
+    """
+
+    global iterations
+    print(f'Training iteration {iterations}')
+    iterations += 1
+    cumulative_rewards = tf.zeros(0, tf.float32)
+
+    for i in trange(len(w1), unit='population_element'):
+
+        game.first_layers[training_agent].set_weights([w1[i], b1[i]])
+        game.out_layers[training_agent].set_weights([w2[i], b2[i]])
+
+        cumulative_reward = tf.Variable(tf.zeros(1))
+        for epoch, game_inputs in enumerate(batch): 
+            _reward = game(game_inputs)[training_agent]
+            cumulative_reward.assign_add(tf.expand_dims(_reward, axis=0))
+
+        cumulative_rewards = tf.concat([cumulative_rewards, cumulative_reward], axis=0)
+
+    return cumulative_rewards
+
+
+training_agent = 0  # Agent to be trained
+
+# Initial weights of layers that are trained
+initial_position = [tf.convert_to_tensor(np.array(np.random.normal(0, 1, size=(1, 128)))),
+                    tf.convert_to_tensor(np.zeros(128)),
+                    tf.convert_to_tensor(np.array(np.random.normal(0, 1, size=(128, 1)))),
+                    tf.convert_to_tensor(np.zeros(1))]
+
+optim_results = tfp.optimizer.differential_evolution_minimize(objective_fn, initial_position=initial_position,
+                                                              max_iterations=MAX_ITERATIONS, seed=0)
+print(optim_results.converged)
+
+# if save_model:
+#   filename = 'weights_'
+#   checkpoint_path = 'saved_model_weights/'
+#   cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+#           save_weights_only=True, verbose=1)
+    
