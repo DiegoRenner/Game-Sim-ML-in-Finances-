24e6a1746207ec644af1d98b97d4723589140e85
Konrad Mueller <mail@konrad-mueller.com>
2021-05-21 16:53:03 +0200

    changes in utils to support train.py

--- full_tf_model/utils.py
+++ full_tf_model/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 
@@ -20,6 +21,34 @@ def create_constant_series(interest_rate, dividend, steps):
     interest_data = [interest_rate] * steps
     dividend_data = [dividend] * steps
     return interest_data, dividend_data
+
+
+def create_random_series(ir_mean, div_mean, steps, ir_sigma=0, div_sigma=0):
+    interest_data = np.random.lognormal(ir_mean, ir_sigma, steps).tolist()
+    dividend_data = np.random.lognormal(div_mean, div_sigma, steps).tolist()
+    return interest_data, dividend_data
+
+
+def create_input_batch(batch_size: int, agent_num: int, steps: int, irm_pool: list, divm_pool: list,
+                       irs_pool: list, divs_pool: list, endow_cash_pool: list, endow_stock_pool: list):
+    """Creates @batch_size number of tensors that represent an input to a single game."""
+
+    ir_means = np.random.choice(irm_pool, batch_size)
+    div_means = np.random.choice(divm_pool, batch_size)
+    ir_sigmas = np.random.choice(irs_pool, batch_size)
+    div_sigmas = np.random.choice(divs_pool, batch_size)
+    endowments_cash = np.random.choice(endow_cash_pool, batch_size)
+    endowments_stock = np.random.choice(endow_stock_pool, batch_size)
+
+    time_series_list = [create_random_series(ir_means[i], div_means[i], steps, ir_sigmas[i],
+                                             div_sigmas[i]) for i in np.arange(batch_size)]
+
+    # All agents have equal cash and stock endowments
+    endowments_cash_list = [[endowments_cash[i]] * agent_num for i in np.arange(batch_size)]
+    endowments_stock_list = [[endowments_stock[i]] * agent_num for i in np.arange(batch_size)]
+
+    return [create_input_tensor(time_series_list[i][0], time_series_list[i][1],
+                                endowments_cash_list[i], endowments_stock_list[i]) for i in np.arange(batch_size)]
 
 
 def create_input_tensor(interest_data, dividend_data, endowments_cash, endowments_stock):
