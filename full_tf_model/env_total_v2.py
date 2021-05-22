5d3f6f49f3b91dc7f545a0761584b1d2509d67b0
DiegoRenner <diego.renner@hotmail.com>
2021-05-22 17:25:14 +0200

    added updating utilities during trading for new env

--- full_tf_model/env_total_v2.py
+++ full_tf_model/env_total_v2.py
@@ -106,7 +106,7 @@ class Game(keras.Model):
             orders_temp = SortedList(key=lambda x: x.price)
 
             for i in np.arange(len(orders)):
-                orders_temp.add(Order(agent_id=orders[i], price=prices[orders[i]]))
+                orders_temp.add(Order(agent_id=orders[i], price=prices[orders[i].agent_id]))
             orders = orders_temp
 
     def call(self, x):
